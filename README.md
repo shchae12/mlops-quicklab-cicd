d4e3e547524401d48cb4a2dab54e363a4f3ddd7b
KennethanCeyer <kenneth@pigno.se>
2024-09-11 13:40:15 +0900

    feat: add default cicd pipelines
    feat: change logo name
    
    feat: change logo name 2

---
+++ README.md
@@ -0,0 +1,9 @@
+<p align="center"><img width="150" src="./assets/logo.png" alt="ops" /></p>
+<h1 align="center">MLOps Quicklab</h1>
+<p align="center">A repository for understanding MLOps CI/CD pipeline.</p>
+
+## Installation
+
+```bash
+git clone https://github.com/KennethanCeyer/mlops-quicklab-cicd.git
+```
---
+++ trainer.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+input_size = 784
+hidden_size = 128
+num_classes = 10
+num_epochs = 5
+batch_size = 100
+learning_rate = 0.001
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+train_dataset = datasets.MNIST(
+    root="data", train=True, transform=transforms.ToTensor(), download=True
+)
+test_dataset = datasets.MNIST(root="data", train=False, transform=transforms.ToTensor())
+
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+
+class NeuralNet(nn.Module):
+    def __init__(self):
+        super(NeuralNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+
+
+model = NeuralNet().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+
+def train():
+    total_step = len(train_loader)
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(train_loader):
+            images = images.reshape(-1, 28 * 28).to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (i + 1) % 100 == 0:
+                print(
+                    f"Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{total_step}], Loss: {loss.item():.4f}"
+                )
+
+
+def save_model():
+    model_path = "model.pth"
+    torch.save(model.state_dict(), model_path)
+    print(f"Model saved to {model_path}")
+
+
+if __name__ == "__main__":
+    train()
+    save_model()
